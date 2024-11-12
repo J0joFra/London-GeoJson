ea4e190655b3df8d8a32c70c52f4e2390a1c63bd
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-11-12 14:56:03 +0100

    Abbreviazioni

--- Script/Map_generator.py
+++ Script/Map_generator.py
@@ -11,7 +11,32 @@ population_data = pd.read_excel(r"C:\Users\JoaquimFrancalanci\OneDrive - ITS Ang
 # Unisci i dati geografici con i dati della popolazione
 lombardia = lombardia.merge(population_data, left_on="name", right_on="Provincia")
 
+# Crea la figura e l'oggetto ax
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
 # Plotta la mappa colorata
-lombardia.plot(column='Popolazione', cmap='OrRd', legend=True)
-plt.title("Popolazione per Provincia in Lombardia")
+lombardia.plot(column='Popolazione', cmap='Greens', legend=True, ax=ax,
+               legend_kwds={'label': "Popolazione (Milioni)", 'orientation': "horizontal"},
+               edgecolor='black')
+
+# Rimuovi gli assi
+ax.set_axis_off()
+
+# Aggiungi le abbreviazioni delle province
+abbreviations = {
+    "Milano": "MI", "Monza e Brianza": "MB", "Bergamo": "BG", "Brescia": "BS", "Como": "CO",
+    "Varese": "VA", "Lecco": "LC", "Lodi": "LO", "Cremona": "CR", "Mantova": "MN", "Pavia": "PV",
+    "Sondrio": "SO", "Cremona": "CR", "Varese": "VA", "Pavia": "PV"
+}
+
+for idx, row in lombardia.iterrows():
+    # Recupera il centroide della provincia per posizionare l'abbreviazione
+    x, y = row['geometry'].centroid.coords[0]
+    abbreviation = abbreviations.get(row['name'], '')
+    ax.text(x, y, abbreviation, fontsize=5, ha='center', color='black')
+
+# Titolo
+plt.title("Popolazione per Provincia in Lombardia", fontsize=16)
+
+# Mostra la mappa
 plt.show()
