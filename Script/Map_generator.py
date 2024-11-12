6fc1ac5172f97b09145c51d3657b20ccd400590f
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-11-12 14:47:34 +0100

    Update

--- Script/Map_generator.py
+++ Script/Map_generator.py
@@ -9,9 +9,9 @@ lombardia = gpd.read_file("Map/Province_Italy.geojson")
 population_data = pd.read_excel(r"C:\Users\JoaquimFrancalanci\OneDrive - ITS Angelo Rizzoli\Desktop\Progetti\Instagram\Dataset\Lombadia 2024.xlsx", sheet_name="Sheet 1")
 
 # Unisci i dati geografici con i dati della popolazione
-lombardia = lombardia.merge(population_data, left_on="name", right_on="province")
+lombardia = lombardia.merge(population_data, left_on="name", right_on="Provincia")
 
 # Plotta la mappa colorata
-lombardia.plot(column='population', cmap='OrRd', legend=True)
+lombardia.plot(column='Popolazione', cmap='OrRd', legend=True)
 plt.title("Popolazione per Provincia in Lombardia")
 plt.show()
